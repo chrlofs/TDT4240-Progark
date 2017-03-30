9953a2543f7505844d229d6d75388c16fa3588c5
Khrall <raymitoro@gmail.com>
2017-03-30 13:26:51 +0200

    Multipeer Connectivity tutorial followed

--- Drop/Drop/multiplayerMenuVC.swift
+++ Drop/Drop/multiplayerMenuVC.swift
@@ -9,13 +9,58 @@
 import Foundation
 import UIKit
 class multiplayerMenuVC: UIViewController{
+    let colorService = ColorServiceManager()
+    
     override func viewDidLoad() {
+        super.viewDidLoad()
+        colorService.delegate = self
     }
+    
+    @IBOutlet weak var connectionsLabel: UILabel!
+    
     @IBAction func back(_ sender: UIButton) {
         backToMenu()
     }
+    
+    @IBAction func redTapped() {
+        self.change(color: .red)
+        colorService.send(colorName: "red")
+    }
+    
+    @IBAction func yellowTapped() {
+        self.change(color: .yellow)
+        colorService.send(colorName: "yellow")
+    }
+    
     func backToMenu(){
         let menuVC = self.storyboard?.instantiateViewController(withIdentifier: "menuVC") as! menuVC
         self.navigationController?.pushViewController(menuVC, animated: true)
+    }
+    
+    func change(color: UIColor) {
+        UIView.animate(withDuration: 0.2) {
+            self.view.backgroundColor = color
+        }
+    }
+}
+
+extension multiplayerMenuVC : ColorServiceManagerDelegate {
+    func connectedDevicesChanged(manager: ColorServiceManager, connectedDevices: [String]) {
+        OperationQueue.main.addOperation {
+            self.connectionsLabel.text = "Connections: \(connectedDevices)"
+        }
+    }
+    
+    func colorChanged(manager: ColorServiceManager, colorString: String) {
+        OperationQueue.main.addOperation {
+            switch colorString {
+            case "red":
+                self.change(color: .red)
+            case "yellow":
+                self.change(color: .yellow)
+            default:
+                NSLog("%@", "Unknown color value received: \(colorString)")
+            }
+        }
     }
 }
