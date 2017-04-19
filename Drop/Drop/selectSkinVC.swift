1371ebcde50467c1f89bafc88d7f28932208e48f
hafager <fagervoll@gmail.com>
2017-04-19 18:56:04 +0200

    Can now Change Player Skin. The list of skins is saved in "skinList" in  ́defaults ́. Fixes #25 The current skin is saved in defaults as "skin". This is an index to the list of skins. Fixes #26 The player can now change the skin in the  ́skinSelect ́ view. Fixes #27

---
+++ Drop/Drop/selectSkinVC.swift
@@ -0,0 +1,56 @@
+//
+//  selectSkinVC.swift
+//  Drop
+//
+//  Created by Håvard Fagervoll on 19/04/2017.
+//  Copyright © 2017 Team15. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class selectSkinVC: UIViewController{
+    let defaults = UserDefaults.standard
+    
+
+    @IBOutlet weak var skinImage: UIImageView!
+    
+    @IBAction func skinLeft(_ sender: Any) {
+        print("left")
+        
+        let skins = defaults.stringArray(forKey: "skinList") ?? [String]()
+        let currentSkinIndex = defaults.integer(forKey: "skin")
+        var newSkinIndex = (currentSkinIndex - 1) % skins.count
+        if (newSkinIndex < 0) {
+            newSkinIndex = newSkinIndex + skins.count
+        }
+        print(newSkinIndex)
+        
+        skinImage.image = UIImage(named: skins[newSkinIndex])
+        defaults.set(newSkinIndex, forKey: "skin")
+        
+    }
+    @IBAction func skinRight(_ sender: Any) {
+        print("right")
+        
+        let skins = defaults.stringArray(forKey: "skinList") ?? [String]()
+        let currentSkinIndex = defaults.integer(forKey: "skin")
+        let newSkinIndex = (currentSkinIndex + 1) % skins.count
+        print(newSkinIndex)
+        
+        skinImage.image = UIImage(named: skins[newSkinIndex])
+        defaults.set(newSkinIndex, forKey: "skin")
+        
+        
+    }
+    
+    override func viewDidLoad() {
+        self.navigationController?.isNavigationBarHidden = true
+
+        let skins = defaults.stringArray(forKey: "skinList") ?? [String]()
+        let currentSkinIndex = defaults.integer(forKey: "skin")
+        print(skins)
+        print(currentSkinIndex)
+        skinImage.image = UIImage(named: skins[currentSkinIndex])
+
+    }
+}
