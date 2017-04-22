055df4009cc7d640a1ff36042abab80f1d4a9121
Christoffer Skar Lofsberg <chris.lofsberg@gmail.com>
2017-04-22 20:04:37 +0200

    Fix sound manager Added sound manager to singletons

---
+++ Drop/soundManager.swift
@@ -0,0 +1,106 @@
+//
+//  soundManager.swift
+//  Drop
+//
+//  Created by Kristoffer Thorset on 27.03.2017.
+//  Copyright © 2017 Team15. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+class soundManager {
+    
+    //AUDIOPLAYERS
+    var fxPlayer: AVAudioPlayer = AVAudioPlayer()
+    var musicPlayer: AVAudioPlayer = AVAudioPlayer()
+    
+    //FLAGS
+    var fxPlaying = false
+    var musicPlaying = false
+    var musicMuted = false
+    var fxMuted = false
+    
+    //SINGLETON
+    static let sharedInstance = soundManager()
+    
+    
+    
+    //USE FXPLAYER
+    func playFx(fileName: String, fileType: String){
+        if !fxMuted{
+            let url = Bundle.main.url(forResource: fileName, withExtension: fileType)!
+            do {
+                fxPlayer = try AVAudioPlayer(contentsOf: url)
+                fxPlayer.prepareToPlay()
+                fxPlayer.play()
+            }
+                
+            catch  { // couldn't load file :(
+            }
+        }
+    }
+    
+    //USE MUSICPLAYER
+    func playMusic(fileName: String, fileType: String){
+        if !musicMuted {
+            let url = Bundle.main.url(forResource: fileName, withExtension: fileType)!
+            do {
+                if !musicPlaying{
+                    musicPlayer = try AVAudioPlayer(contentsOf: url)
+                    musicPlayer.numberOfLoops = -1
+                    musicPlayer.prepareToPlay()
+                    musicPlayer.play()
+                    if musicPlayer.isPlaying{
+                        musicPlaying = true
+                    }
+                }
+            }
+                
+            catch  {
+                // couldn't load file :(
+            }
+        }
+        
+    }
+    
+    
+    //STOP FXPLAYER
+    func stopFx(){
+        fxPlaying = false
+        fxPlayer.stop()
+    }
+    
+    //MUTE FXPLAYER
+    func muteFX(){
+        fxMuted = true
+    }
+    
+    //UNMUTE FXPLAYER
+    func unmuteFX(){
+        fxMuted = false
+    }
+    
+    
+    //STOP MUSICPLAYER
+    func stopMusic(){
+        musicPlaying = false
+        musicPlayer.stop()
+    }
+    
+    //MUTE MUSICPLAYER
+    func muteMusic(){
+        if musicPlaying{
+            musicPlayer.volume = 0
+            musicMuted = true
+        }
+    }
+    
+    //UNMUTE MUSICPLAYER
+    func unmuteMusic(){
+        if musicPlaying{
+            musicPlayer.volume = 1
+            musicMuted = false
+        }
+    }
+}
